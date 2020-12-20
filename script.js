400981b49cb64df29d3ecc7fc546680959e501e9
Robinsstudio <robin.dosanjos@akur8-tech.com>
2020-12-20 17:54:24 +0100

    Create Algorithm function

--- script.js
+++ script.js
@@ -1,4 +1,50 @@
-const generatePassword = function() {
+function copyToClipboard(value) {
+	const hiddenElement = document.createElement('input');
+	hiddenElement.style.background = 'transparent';
+	hiddenElement.value = value;
+
+	document.body.appendChild(hiddenElement);
+	hiddenElement.select();
+	document.execCommand('copy');
+	document.body.removeChild(hiddenElement);
+}
+
+const siteField = document.querySelector('#site');
+const passwordField = document.querySelector('#password');
+const numberCharsField = document.querySelector('#numberChars');
+const specialCharsField = document.querySelector('#specialChars');
+const generatedPasswordField = document.querySelector('#generatedPassword');
+const copyButton = document.querySelector('#copy');
+const eyes = document.querySelectorAll('.eye');
+
+const updateProgressFn = e => console.log(e);
+
+document.addEventListener('input', () => {
+	algorithms['SHA-512'](
+		siteField.value + passwordField.value,
+		parseInt(numberCharsField.value),
+		specialCharsField.checked,
+		updateProgressFn
+	).then(password => generatedPasswordField.value = password);
+});
+
+copyButton.addEventListener('click', () => copyToClipboard(generatedPasswordField.value));
+
+Array.from(eyes).forEach(eye => {
+	eye.addEventListener('click', () => {
+		const input = eye.parentElement.querySelector('input');
+		input.type = eye.classList.contains('crossed') ? 'password' : 'text';
+		eye.classList.toggle('crossed');
+	});
+});
+
+function Algorithm(algorithmCallback) {
+	return function(text, size, specialChars, progressCallback) {
+		return algorithmCallback(text, size, specialChars, progressCallback);
+	}
+}
+
+const sha512 = function() {
 	const charSet64 = [
 		'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
 		'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
@@ -25,39 +71,6 @@ const generatePassword = function() {
 
 }();
 
-function copyToClipboard(value) {
-	const hiddenElement = document.createElement('input');
-	hiddenElement.style.background = 'transparent';
-	hiddenElement.value = value;
-
-	document.body.appendChild(hiddenElement);
-	hiddenElement.select();
-	document.execCommand('copy');
-	document.body.removeChild(hiddenElement);
-}
-
-const siteField = document.querySelector('#site');
-const passwordField = document.querySelector('#password');
-const numberCharsField = document.querySelector('#numberChars');
-const specialCharsField = document.querySelector('#specialChars');
-const generatedPasswordField = document.querySelector('#generatedPassword');
-const copyButton = document.querySelector('#copy');
-const eyes = document.querySelectorAll('.eye');
-
-document.addEventListener('input', () => {
-	generatePassword(
-		siteField.value + passwordField.value,
-		parseInt(numberCharsField.value),
-		specialCharsField.checked
-	).then(password => generatedPasswordField.value = password);
-});
-
-copyButton.addEventListener('click', () => copyToClipboard(generatedPasswordField.value));
-
-Array.from(eyes).forEach(eye => {
-	eye.addEventListener('click', () => {
-		const input = eye.parentElement.querySelector('input');
-		input.type = eye.classList.contains('crossed') ? 'password' : 'text';
-		eye.classList.toggle('crossed');
-	});
-});
+const algorithms = {
+	'SHA-512': Algorithm(sha512)
+};
