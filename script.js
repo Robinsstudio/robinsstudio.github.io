d4d70b1e052855505cc41f74ba7bdcd81f27d9d6
Robinsstudio <robin.dosanjos@akur8-tech.com>
2021-03-22 20:54:22 +0100

    Setting bcrypt as the new recommended algorithm (#1)

--- script.js
+++ script.js
@@ -1,4 +1,114 @@
-const generatePassword = function() {
+const siteField = document.querySelector('#site');
+const passwordField = document.querySelector('#password');
+const numberCharsField = document.querySelector('#numberChars');
+const specialCharsField = document.querySelector('#specialChars');
+const algorithmField = document.querySelector('#algorithm input');
+const algorithmValue = document.querySelector('#algorithm .value');
+const generatedPasswordField = document.querySelector('#generatedPassword');
+const copyButton = document.querySelector('#copy');
+const eyes = document.querySelectorAll('.eye');
+const progressBarText = document.querySelector('.progress-bar-text');
+const progressBarOverlay = document.querySelector('.progress-bar-overlay');
+
+const SITE = 'SITE';
+const NUMBER_CHARS = 'NUMBER_CHARS';
+
+let password = '';
+
+function updateProgress(progress) {
+	const percentage = progress * 100;
+
+	progressBarText.textContent = `Génération du mot de passe... (${Math.round(percentage)} %)`;
+	progressBarOverlay.style.width = `${percentage}%`;
+}
+
+function updateMessage(message) {
+	progressBarText.textContent = message;
+}
+
+function copyToClipboard(value) {
+	const fakeElement = document.createElement('input');
+	fakeElement.value = value;
+
+	document.body.appendChild(fakeElement);
+	fakeElement.select();
+	document.execCommand('copy');
+	document.body.removeChild(fakeElement);
+
+	updateMessage('Mot de passe copié');
+}
+
+const generatePassword = (function() {
+	let computing = false;
+
+	return function() {
+		if (!computing) {
+			computing = true;
+
+			setLocalStorage(SITE, siteField.value);
+			setLocalStorage(NUMBER_CHARS, numberCharsField.value);
+
+			algorithms[algorithmField.value].run(
+				siteField.value + passwordField.value,
+				specialCharsField.checked,
+				updateProgress
+			).then(password => {
+				updatePassword(password);
+				updateMessage('Mot de passe généré');
+
+				computing = false;
+			});
+		}
+	}
+})();
+
+function updatePassword(pass) {
+	password = pass;
+	updateSize();
+}
+
+function updateSize() {
+	const size = parseInt(numberCharsField.value);
+	generatedPasswordField.value = password.slice(0, size);
+}
+
+function getLocalStorage(itemName, defaultValue) {
+	return window.localStorage.getItem(itemName) || defaultValue;
+}
+
+function setLocalStorage(itemName, value) {
+	return window.localStorage.setItem(itemName, value);
+}
+
+siteField.value = getLocalStorage(SITE, '');
+numberCharsField.value = parseInt(getLocalStorage(NUMBER_CHARS, '30'));
+
+document.addEventListener('keydown', event => {
+	if (event.key === 'Enter') {
+		generatePassword();
+	}
+});
+
+algorithmField.addEventListener('input', event => {
+	const { value } = event.target;
+	if (value) {
+		algorithmValue.textContent = algorithms[value].name;
+	}
+});
+
+numberCharsField.addEventListener('input', () => updateSize());
+
+copyButton.addEventListener('click', () => copyToClipboard(generatedPasswordField.value));
+
+Array.from(eyes).forEach(eye => {
+	eye.addEventListener('click', () => {
+		const input = eye.parentElement.querySelector('input');
+		input.type = eye.classList.contains('crossed') ? 'password' : 'text';
+		eye.classList.toggle('crossed');
+	});
+});
+
+const sha512 = function() {
 	const charSet64 = [
 		'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
 		'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
@@ -14,50 +124,33 @@ const generatePassword = function() {
 		'~', '£', '€', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
 	];
 
-	return function(text, size, specialChars) {
+	return function(text, specialChars) {
 		const chars = specialChars ? charSet128 : charSet64;
 		const divider = specialChars ? 2 : 4;
 
-		return crypto.subtle.digest('SHA-512', new TextEncoder().encode(text)).then(buffer => {
-			return Array.from(new Uint8Array(buffer)).map(value => chars[Math.floor(value / divider)]).join('').substring(0, size);
+		return window.crypto.subtle.digest('SHA-512', new TextEncoder().encode(text)).then(buffer => {
+			return Array.from(new Uint8Array(buffer)).map(value => chars[Math.floor(value / divider)]).join('');
 		});
 	}
 
-}();
+};
 
-function copyToClipboard(value) {
-	const hiddenElement = document.createElement('input');
-	hiddenElement.style.background = 'transparent';
-	hiddenElement.value = value;
+const bcrypt = function() {
+	const DUMMY_SALT = 'RobinRobinRobinRobinRe';
+	const prefix = `$2a$15$${DUMMY_SALT}`;
 
-	document.body.appendChild(hiddenElement);
-	hiddenElement.select();
-	document.execCommand('copy');
-	document.body.removeChild(hiddenElement);
-}
+	return function(text, _, progressCallback) {
+		return new Promise(resolve => {
+			const resolvePromise = (error, hash) => resolve({ error, hash });
+			window.dcodeIO.bcrypt.hash(text, prefix, resolvePromise, progressCallback)
+		}).then(({ hash }) => hash.slice(prefix.length));
+	};
+};
 
-const siteField = document.querySelector('#site');
-const passwordField = document.querySelector('#password');
-const numberCharsField = document.querySelector('#numberChars');
-const specialCharsField = document.querySelector('#specialChars');
-const generatedPasswordField = document.querySelector('#generatedPassword');
-const copyButton = document.querySelector('#copy');
-const eyes = document.querySelectorAll('.eye');
-
-document.addEventListener('input', () => {
-	generatePassword(
-		siteField.value + passwordField.value,
-		parseInt(numberCharsField.value),
-		specialCharsField.checked
-	).then(password => generatedPasswordField.value = password);
-});
-
-copyButton.addEventListener('click', () => copyToClipboard(generatedPasswordField.value));
-
-Array.from(eyes).forEach(eye => {
-	eye.addEventListener('click', () => {
-		const input = eye.parentElement.querySelector('input');
-		input.type = eye.classList.contains('crossed') ? 'password' : 'text';
-		eye.classList.toggle('crossed');
-	});
-});
+const algorithms = [{
+	name: 'SHA-512',
+	run: sha512()
+}, {
+	name: 'bcrypt',
+	run: bcrypt()
+}];
