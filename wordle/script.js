666f676da063173bb831eb2e9574b0ccb2b94469
Robinsstudio <robin.dosanjos@akur8.com>
2022-10-02 15:58:10 +0200

    Rotation animation

--- wordle/script.js
+++ wordle/script.js
@@ -1,43 +1,63 @@
 const ROWS = 6;
 const COLUMNS = 5;
+const OPENER = 'slate';
 
 const CELL_STATES = ['absent', 'present', 'correct'];
 
 const cells = Array.from({ length: ROWS }, () => Array.from({ length: COLUMNS }, () => 0));
+let currentRow = 0;
 
-const grid = document.querySelector('.grid');
+function initialize() {
+    const grid = document.querySelector('.grid');
 
-for (let i = 0; i < ROWS; i++) {
-    const row = document.createElement('div');
-    row.classList.add('row');
+    for (let i = 0; i < ROWS; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
 
-    for (let j = 0; j < COLUMNS; j++) {
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-        cell.dataset.row = i;
-        cell.dataset.col = j;
+        for (let j = 0; j < COLUMNS; j++) {
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            cell.dataset.row = i;
+            cell.dataset.col = j;
 
-        row.appendChild(cell);
+            row.appendChild(cell);
+        }
+
+        grid.appendChild(row);
     }
 
-    grid.appendChild(row);
+    grid.addEventListener('click', event => {
+        const cell = event.target;
+
+        if (!cell.classList.contains('cell')) {
+            return;
+        }
+
+        const row = cell.dataset.row;
+        const col = cell.dataset.col;
+        const state = CELL_STATES[cells[row][col]];
+        const newStateIndex = (cells[row][col] + 1) % CELL_STATES.length;
+        const newState = CELL_STATES[newStateIndex];
+
+        cells[row][col] = newStateIndex;
+
+        cell.classList.remove(state);
+        cell.classList.add(newState);
+    });
 }
 
-grid.addEventListener('click', event => {
-    const cell = event.target;
+function showNextWord() {
+    const word = OPENER;
 
-    if (!cell.classList.contains('cell')) {
-        return;
+    const row = document.querySelectorAll('.row')[currentRow++];
+    const children = row.children;
+
+    for (let i = 0; i < children.length; i++) {
+        const child = children[i];
+        child.classList.add('absent');
+        child.textContent = word[i];
     }
+}
 
-    const row = cell.dataset.row;
-    const col = cell.dataset.col;
-    const state = CELL_STATES[cells[row][col]];
-    const newStateIndex = (cells[row][col] + 1) % CELL_STATES.length;
-    const newState = CELL_STATES[newStateIndex];
-
-    cells[row][col] = newStateIndex;
-
-    cell.classList.remove(state);
-    cell.classList.add(newState);
-});
+initialize();
+showNextWord();
